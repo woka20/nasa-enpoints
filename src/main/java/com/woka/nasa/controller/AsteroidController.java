2b38577e6cced49ac272df11d5fcc7ff37a52e7c
Woka Aditama <woka@ptmitrabhakti.com>
2024-02-25 10:00:20 +0700

    add restTemplate

--- src/main/java/com/woka/nasa/controller/AsteroidController.java
+++ src/main/java/com/woka/nasa/controller/AsteroidController.java
@@ -1,5 +1,21 @@
 package com.woka.nasa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.woka.nasa.service.AsteroidService;
+
+@RestController
 public class AsteroidController {
-    
+
+    private AsteroidService asteroidService;
+
+    public AsteroidController(AsteroidService asteroidService) {
+        this.asteroidService = asteroidService;
+    }
+
+    @GetMapping("/data-nasa-asteroid")
+    public String getDataFromThirdParty() {
+        return asteroidService.fetchDataFromThirdParty();
+    }
 }
--- src/main/java/com/woka/nasa/service/AsteroidService.java
+++ src/main/java/com/woka/nasa/service/AsteroidService.java
@@ -1,5 +1,23 @@
 package com.woka.nasa.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class AsteroidService {
-    
+
+    private final String API_URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY";
+
+    private final RestTemplate restTemplate ;
+
+    public AsteroidService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String fetchDataFromThirdParty() {
+        // Make GET request to third-party API
+        String response = restTemplate.getForObject(API_URL, String.class);
+        return response;
+    }
 }
+

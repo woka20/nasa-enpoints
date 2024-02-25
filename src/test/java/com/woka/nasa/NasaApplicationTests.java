252bffd84cda8a8ea73997d90814331aae15a7be
Woka Aditama <woka@ptmitrabhakti.com>
2024-02-25 08:57:47 +0700

    initial

---
+++ src/test/java/com/woka/nasa/NasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woka.nasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
